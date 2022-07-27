fbc73dbc6ce57624cabbca11bf5c756da5e9864b
Leland Batey <lelandbatey@lelandbatey.com>
2022-07-27 13:33:18 -0700

    Improve map display and binning

--- create_maps.py
+++ create_maps.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+'''
+create_maps.py makes pretty maps of GPS routes. Reads JSON-lines data on STDIN,
+data formatted like so:
+
+    {"latitude": 48.724511, "longitude": -119.446908, "timestamp_utc": 1467914535}
+    {"latitude": 48.724283, "longitude": -119.4473, "timestamp_utc": 1467914547}
+
+It then uses the Google maps API to find the probable road-based routes between
+those coordinates (if those coordinates are further than .5 kilometers apart)
+and the routes implied by the coordinates on a map.
+'''
 import googlemaps
 import staticmap
 
@@ -7,37 +19,53 @@ import json
 import math
 import sys
 
+from typing import List, Any, Dict, Tuple
+
 # Pirated google maps API key (from an example code sample published by Google)
 GMAPS_APIKEY = "AIz" + "aSyA3gqF4a2G0bcRG7J" + "gzAwo40iVStrSv2OM"
 
+Coord = Tuple[float, float]
+
+
+class TimeLocation:
+    def __init__(self, lat=None, lng=None, moment=None):
+        '''TimeLocation is both a time and place.'''
+        # lat/lng are latitude longitude expressed in Decimal Degrees (DD)
+        self.lat = lat
+        self.lng = lng
+        # moment is epoch timestamp in seconds
+        self.moment: int = moment
+
+    @staticmethod
+    def fromrow(d):
+        lat = d['latitude']
+        lng = d['longitude']
+        moment = d['timestamp_utc']
+        return TimeLocation(lat, lng, moment)
+
+    def latlngpoint(self):
+        return [self.lat, self.lng]
+
+    def dt(self) -> datetime.datetime:
+        return datetime.datetime.fromtimestamp(self.moment)
+
 
 def color_hash(obj):
-    # https://stackoverflow.com/a/4382138
-    kelly_colors_hex = [
-        '#FFB300',  # Vivid Yellow
-        '#803E75',  # Strong Purple
-        '#FF6800',  # Vivid Orange
-        '#A6BDD7',  # Very Light Blue
-        '#C10020',  # Vivid Red
-        '#CEA262',  # Grayish Yellow
-        '#817066',  # Medium Gray
-
-        # The following don't work well for people with defective color vision
-        '#007D34',  # Vivid Green
-        '#F6768E',  # Strong Purplish Pink
-        '#00538A',  # Strong Blue
-        '#FF7A5C',  # Strong Yellowish Pink
-        '#53377A',  # Strong Violet
-        '#FF8E00',  # Vivid Orange Yellow
-        '#B32851',  # Strong Purplish Red
-        '#F4C800',  # Vivid Greenish Yellow
-        # '#7F180D',  # Strong Reddish Brown
-        '#93AA00',  # Vivid Yellowish Green
-        '#593315',  # Deep Yellowish Brown
-        # '#F13A13',  # Vivid Reddish Orange
-        '#232C16',  # Dark Olive Green
+    # Leland colors (picked via Munsell color picker, found here: https://colorizer.org/)
+    leland_colors_hex = [
+        # Very bright colors
+        '#FF00A6',  # Magenta
+        '#FF0000',  # Red
+        '#FFDD00',  # Yellow
+        '#00FF00',  # Green
+        '#00FFFF',  # Cyan
+        '#FF9300',  # Orange
+        # Darker colors
+        '#004500',  # Dark green
+        '#002492',  # Dark blue
+        '#8b0000',  # Dark red
     ]
-    return kelly_colors_hex[binascii.crc32(str(obj).encode('utf-8')) % len(kelly_colors_hex)]
+    return leland_colors_hex[binascii.crc32(str(obj).encode('utf-8')) % len(leland_colors_hex)]
 
 
 # Below taken from here:
@@ -45,7 +73,6 @@ def color_hash(obj):
 def distance_on_unit_sphere(lat1, long1, lat2, long2):
     """Calculates the distance between two lat/long points. Returns distance in
     meters."""
-
     lat1, long1 = float(lat1), float(long1)
     lat2, long2 = float(lat2), float(long2)
     # Convert latitude and longitude to
@@ -61,13 +88,11 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
     theta2 = long2 * degrees_to_radians
 
     # Compute spherical distance from spherical coordinates.
-
     # For two locations in spherical coordinates
     # (1, theta, phi) and (1, theta, phi)
     # cosine( arc length ) =
     #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
     # distance = rho * arc length
-
     cos = (
         math.sin(phi1) * math.sin(phi2) * math.cos(theta1 - theta2) +
         math.cos(phi1) * math.cos(phi2)
@@ -76,14 +101,11 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
     try:
         arc = math.acos(cos)
     except Exception as e:
-        #print("Cos:", cos)
-        #print(e)
         raise e
-
     return arc * 6378100
 
 
-def decode_polyline(point_str):
+def decode_polyline(point_str) -> List[Coord]:
     '''Decodes a polyline that has been encoded using Google's algorithm
     http://code.google.com/apis/maps/documentation/polylinealgorithm.html
     https://gist.github.com/signed0/2031157
@@ -96,62 +118,53 @@ def decode_polyline(point_str):
     :returns: List of 2-tuples where each tuple is (latitude, longitude)
     :rtype: list
     '''
-
     # sone coordinate offset is represented by 4 to 5 binary chunks
     coord_chunks = [[]]
     for char in point_str:
-
         # convert each character to decimal from ascii
         value = ord(char) - 63
-
         # values that have a chunk following have an extra 1 on the left
         split_after = not (value & 0x20)
         value &= 0x1F
 
         coord_chunks[-1].append(value)
-
         if split_after:
             coord_chunks.append([])
 
     del coord_chunks[-1]
-
     coords = []
 
     for coord_chunk in coord_chunks:
         coord = 0
-
         for i, chunk in enumerate(coord_chunk):
             coord |= chunk << (i * 5)
-
         #there is a 1 on the right if the coord is negative
         if coord & 0x1:
             coord = ~coord  #invert
         coord >>= 1
         coord /= 100000.0
-
         coords.append(coord)
-
     # convert the 1 dimensional list to a 2 dimensional list and offsets to
     # actual values
-    points = []
+    points: List[Coord] = []
     prev_x = 0
     prev_y = 0
     for i in range(0, len(coords) - 1, 2):
         if coords[i] == 0 and coords[i + 1] == 0:
             continue
-
         prev_x += coords[i + 1]
         prev_y += coords[i]
         # a round to 6 digits ensures that the floats are the same as when
         # they were encoded
-        points.append([round(prev_y, 6), round(prev_x, 6)])
-
+        points.append((round(prev_y, 6), round(prev_x, 6)))
     return points
 
 
-def flatten_routes_points(directionsresponse):
-    # print(directionsresponse)
-    flatpoints = list()
+def flatten_routes_points(directionsresponse) -> List[Coord]:
+    '''Accepts a `directionsresponse`, which is the response from calling the
+    Google Maps `directions()` API. Returns a list of tuples, where each tuple
+    is a lat-lng pair. '''
+    flatpoints: List[Coord] = list()
     for route in directionsresponse:
         directionslegs = route['legs']
         for leg in directionslegs:
@@ -161,46 +174,44 @@ def flatten_routes_points(directionsresponse):
     return flatpoints
 
 
-def bin_by_day(rows):
+def bin_by_day(tlocs: List[TimeLocation]) -> Dict[str, List[TimeLocation]]:
     days = {}
-    for row in rows:
-        ts = datetime.datetime.fromtimestamp(row['timestamp_utc'])
+    for tl in tlocs:
+        ts = datetime.datetime.fromtimestamp(tl.moment)
         ts_str = ts.strftime("%Y %m %d")
         if not ts_str in days:
             days[ts_str] = list()
-        days[ts_str].append(row)
+        days[ts_str].append(tl)
     return days
 
 
-def draw_rows(m: staticmap.StaticMap, gmaps, rows, linecolor='red', markercolor='green'):
-    points = [[cur['latitude'], cur['longitude']] for cur in rows]
+def draw_tlocs(
+    m: staticmap.StaticMap,
+    tlocs: List[TimeLocation],
+    orig_tlocs: List[TimeLocation],
+    linecolor='red',
+    markercolor='green',
+):
+    def tlocs_to_lines(_tlocs: List[TimeLocation]):
+        _lpairs = list()
+        for idx in range(len(_tlocs) - 1):
+            cur = _tlocs[idx].latlngpoint()
+            nxt = _tlocs[idx + 1].latlngpoint()
+            coordpair = [
+                # I don't know why, but this komoot/staticmap library uses longitude-first coordinates :(
+                [cur[1], cur[0]],
+                [nxt[1], nxt[0]],
+            ]
+            _lpairs.append(coordpair)
+        return _lpairs
 
-    smoothpoints = list()
-    for idx in range(len(points) - 1):
-        cur = points[idx]
-        nxt = points[idx + 1]
-        if distance_on_unit_sphere(*cur, *nxt) > 500:
-            directions = gmaps.directions(cur, nxt)
-            print('Getting directions')
-            flatpoints = flatten_routes_points(directions)
-            smoothpoints += flatpoints
-        else:
-            smoothpoints += [cur, nxt]
-
-    lpairs = list()
-
-    for idx in range(len(smoothpoints) - 1):
-        cur = smoothpoints[idx]
-        nxt = smoothpoints[idx + 1]
-        coordpair = [
-            # I don't know why, but this komoot/staticmap library uses longitude-first coordinates :(
-            [cur[1], cur[0]],
-            [nxt[1], nxt[0]],
-        ]
-        lpairs.append(coordpair)
+    linepairs = list()
+    linepairs = tlocs_to_lines(tlocs)
+    # We use normal line formatting because the drawing function will flip the format
+    orig_points = [tloc.latlngpoint() for tloc in orig_tlocs]
 
     def draw_lines(color, thickness):
-        for pair in lpairs:
+        for pair in linepairs:
             coords = pair
             line = staticmap.Line(coords, color, thickness)
             m.add_line(line)
@@ -209,13 +220,64 @@ def draw_rows(m: staticmap.StaticMap, gmaps, rows, linecolor='red', markercolor=
     draw_lines(linecolor, 4)
 
     def draw_rawpoints(color, thickness):
-        for point in points:
+        for point in orig_points:
             pnt = [point[1], point[0]]
             marker = staticmap.CircleMarker(pnt, color, thickness)
             m.add_marker(marker)
 
     draw_rawpoints('white', 8)
     draw_rawpoints(markercolor, 5)
+
+
+def clamp_end_before_midnight(
+    start_t: datetime.datetime, end_t: datetime.datetime
+) -> Tuple[datetime.datetime, datetime.datetime]:
+    '''
+    If the time difference between start and end would span multiple days, we
+    don't want to smoothly interpolate between then since that would imply
+    continuous travel, even through the middle of the night, which doesn't
+    match up with what we'd intuit. Instead, we clamp the end time to be 1
+    second before midnight on the day of the start, and we assume that there's
+    just missing data between then and the start of the later day.
+    '''
+    if start_t.day != end_t.day:
+        end_t = datetime.datetime(
+            year=start_t.year,
+            day=start_t.day,
+            month=start_t.month,
+            hour=23,
+            minute=59,
+            second=59,
+        )
+    return start_t, end_t
+
+
+def interpolate_timelocations(
+    gmaps: googlemaps.Client,
+    tlocs: List[TimeLocation],
+) -> List[TimeLocation]:
+    ''' Given a list of TimeLocations, interpolate between the two
+    timelocations using the Googlemaps directions() API. '''
+    interpolated_tlocs: List[TimeLocation] = list()
+    for idx in range(len(tlocs) - 1):
+        cur: TimeLocation = tlocs[idx]
+        nxt: TimeLocation = tlocs[idx + 1]
+        interpolated_tlocs.append(cur)
+        if distance_on_unit_sphere(*(cur.latlngpoint()), *(nxt.latlngpoint())) > 500:
+            print('Getting directions')
+            directions = gmaps.directions(cur.latlngpoint(), nxt.latlngpoint())
+            flatpoints = flatten_routes_points(directions)
+            # If the timediff would span a midnight boundary, the we assume
+            # that there's a time-jump and clamp the duration interp
+            start_t, end_t = clamp_end_before_midnight(cur.dt(), nxt.dt())
+            per_point_timediff = (end_t - start_t) / (len(flatpoints) + 1)
+            for pidx, point in enumerate(flatpoints):
+                offset = (pidx + 1) * per_point_timediff
+                newtime: datetime.datetime = start_t + offset
+                newtloc = TimeLocation(lat=point[0], lng=point[1], moment=newtime.timestamp())
+                interpolated_tlocs.append(newtloc)
+    interpolated_tlocs.append(tlocs[-1])
+    return interpolated_tlocs
 
 
 def main():
@@ -229,11 +291,16 @@ def main():
     rows = [r for r in rows if not 'error' in r or not r['error']]
     rows = sorted(rows, key=lambda x: x['timestamp_utc'])
 
-    dayrows = bin_by_day(rows)
+    tlocs = [TimeLocation.fromrow(row) for row in rows]
+    interp_tlocs = interpolate_timelocations(gmaps, tlocs)
 
-    for day, drows in dayrows.items():
+    orig_day_tlocs = bin_by_day(tlocs)
+    day_tlocs = bin_by_day(interp_tlocs)
+
+    for day, dtlocs in day_tlocs.items():
         color = color_hash(day)
-        draw_rows(m, gmaps, drows, linecolor=color, markercolor=color)
+        orig_dtlocs = orig_day_tlocs.get(day, list())
+        draw_tlocs(m, dtlocs, orig_dtlocs, linecolor=color, markercolor=color)
 
     image = m.render()
     output_name = 'map.png'
